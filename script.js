da760aa094701c758bc06559e1c654817f716a47
sunkariabhiram90-crypto <sunkariabhiram90@gmail.com>
2026-06-04 21:45:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+//Task 1
+let studentName = "Abhiram.S";
+let studentAge = 22;
+let studentCourse = "Java Full Stack";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+//Task 2
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+
+company = "Google"; //let and var can be reassigned is possible but not for const
+salary = 30000;
+
+console.log(company);
+console.log(salary);
+console.log(country);
+
+
+//Task 3
+let userName = prompt("What is your name?");
+
+console.log("Welcome " + userName);
+
+//Task 4
+let knowsJavaScript = confirm("Do you have JavaScript knowledge?");
+
+console.log(knowsJavaScript);
+
+
+//Task 5
+
+let studentName1 = prompt("Enter Student Name:");
+let studentAge1 = prompt("Enter Student Age:");
+let studentCity1 = prompt("Enter Student City:");
+
+console.log("Name : " + studentName1);
+console.log("Age : " + studentAge1);
+console.log("City : " + studentCity1);
+
+//Task 6
+let course = "JavaScript";
+
+alert("Welcome to " + course);
+
+//Task 7
+document.writeln("Hello Student.   ");
+document.writeln("Welcome to JavaScript");
+
+//Task 8
+let favoriteFood = "Chicken Biryani";
+let favoriteColor = "Black";
+let favoriteAnime = "I Parry Everything";
+
+console.log("Food : " + favoriteFood);
+console.log("Color : " + favoriteColor);
+console.log("Anime : " + favoriteAnime);
+
+//Task 9
+let name1 = "Sai Krishna";
+let age1 = 32;
+let phone1 = "856789012";
+let email1 = "abhiram.s@gmail.com";
+
+console.log("Name : " + name1);
+console.log("Age : " + age1);
+console.log("Phone : " + phone1);
+console.log("Email : " + email1);
+
+//Task 10
+let name2 = prompt("Enter your Name:");
+let course2 = prompt("Enter your Course:");
+
+alert("Hello " + name2 + ", Welcome to " + course2 + " Course");
